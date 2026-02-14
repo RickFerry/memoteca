e7617093989de538348d9d04c1c797b6cc1107ab
Ricardo <rick_ferry1@hotmail.com>
2026-02-14 01:11:31 -0300

    update CriarPensamento component to bind input values and display previews

--- src/app/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-criar-pensamento',
   templateUrl: './criar-pensamento.component.html',
   styleUrls: ['./criar-pensamento.component.css']
 })
-export class CriarPensamentoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class CriarPensamentoComponent {
+  pensamento = {
+    id: 1,
+    conteudo: 'Aprendendo angular',
+    autoria: 'Dev',
+    modelo: ''
   }
 
+  constructor() {
+  }
 }
