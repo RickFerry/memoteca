e1f252bd415a120b8aa0637015b79e439b5fddf3
Ricardo <rick_ferry1@hotmail.com>
2026-02-14 00:53:09 -0300

    add CriarPensamento component and integrate it into the app layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import {AppComponent} from './app.component';
 import {CabecalhoComponent} from './app/componentes/cabecalho/cabecalho.component';
 import {NgOptimizedImage} from "@angular/common";
 import {RodapeComponent} from './app/componentes/rodape/rodape.component';
+import { CriarPensamentoComponent } from './app/componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabecalhoComponent,
-    RodapeComponent
+    RodapeComponent,
+    CriarPensamentoComponent
   ],
   imports: [
     BrowserModule,
